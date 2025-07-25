a23e19d4b334d0fdeb979da3d6de3a3661621052
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2025-07-25 12:24:47 +0530

    second commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT:'#000',
+          100: '#000319'
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
