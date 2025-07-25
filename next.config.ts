30db06d25f9914a38bacdfdb198aae9282ec8474
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2025-07-25 14:24:58 +0530

    spotlight animation

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  theme: {
-    extend: {
-      colors: {
-        black: {
-          DEFAULT:'#000',
-          100: '#000319'
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
